2e15698ecfe948c5047aaf0335806c905747f5c0
JackPadalino <pada0867@gmail.com>
2024-03-25 12:53:52 -0400

    commented out unused code for provider temporarily

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import App from "./App.tsx";
+// import { Provider } from "react-redux";
+import App from "./components/App/App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    {/* <Provider> */}
+  <>
+    {/* <Provider store={store}> */}
     <App />
     {/* </Provider> */}
-  </React.StrictMode>
+  </>
 );
