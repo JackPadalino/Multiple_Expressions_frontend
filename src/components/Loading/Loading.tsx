cae160b23a2d4fd243b3089089b7d683ec7e13b9
JackPadalino <pada0867@gmail.com>
2024-03-25 12:53:23 -0400

    added components directory

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import "./loading.css";
+
+const Loading = () => {
+  return (
+    <Box className="loadingMainContainer">
+      <h2 className="loadingTitle">Patience...</h2>
+    </Box>
+  );
+};
+
+export default Loading;
