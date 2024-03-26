73dd4072a9d8892162114f94bff313d8134ff752
JackPadalino <pada0867@gmail.com>
2024-03-26 16:17:37 -0400

    updated for use with typescript

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -3,21 +3,17 @@ import artistReducer from "./artistSlice";
 import musicReducer from "./musicSlice";
 import waveformReducer from "./waveformSlice";
 import mobileViewReducer from "./mobileViewSlice";
-import usersReducer from "./usersSlice";
 import chatReducer from "./chatSlice";
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     artists: artistReducer,
     music: musicReducer,
     waveform: waveformReducer,
     mobileView: mobileViewReducer,
-    users: usersReducer,
     chat: chatReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-export default store;
