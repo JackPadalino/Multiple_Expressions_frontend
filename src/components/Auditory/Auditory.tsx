3b42a23215f9e7212137f604e012ddcb5a62facb
JackPadalino <pada0867@gmail.com>
2024-04-29 12:10:54 -0400

    added a simple search bar for artists, tracks, and tags - next step is to display search results

--- src/components/Auditory/Auditory.tsx
+++ src/components/Auditory/Auditory.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 import { useAppSelector, useAppDispatch } from "../../store/hooks";
 import { Box } from "@mui/material";
 
@@ -18,11 +18,14 @@ const Auditory = () => {
   const { storeTracks } = useAppSelector((state) => state.music);
 
   // pagiation variables
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState<number>(1);
   const postsPerPage = 4;
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentTracks = storeTracks.slice(indexOfFirstPost, indexOfLastPost);
+
+  // search variables
+  const [searchValue, setSearchValue] = useState<string>("");
 
   const handleChange = (event: ChangeEvent<unknown>, value: number) => {
     setCurrentPage(value);
@@ -33,21 +36,48 @@ const Auditory = () => {
     dispatch(setStoreWaveformTrack(track));
   };
 
+  const handleSeachChange = (e: FormEvent<HTMLInputElement>) => {
+    setSearchValue(e.currentTarget.value.toLowerCase());
+  };
+
+  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const filteredTracks = storeTracks.filter(
+      (track) =>
+        track.title.toLowerCase() === searchValue ||
+        track.artists
+          .map((artist) => artist.name.toLowerCase())
+          .includes(searchValue) ||
+        track.tags.map((tag) => tag.title.toLowerCase()).includes(searchValue)
+    );
+    console.log(filteredTracks);
+    setSearchValue("");
+  };
+
   if (Object.keys(storeTracks).length == 0) return null;
   return (
     <>
       <Box className="auditoryMainContainer">
-        <Box>
-          <Box className="auditoryTracksDiv">
-            {currentTracks.map((track) => (
-              <SingleTrack
-                key={track.id}
-                track={track}
-                handlePlay={handlePlay}
-              />
-            ))}
-          </Box>
+        {/* <Filters /> */}
+        <Box className="filtersContainer">
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              placeholder="Search artists/tracks/tags"
+              style={{ color: "black" }}
+              value={searchValue}
+              onChange={handleSeachChange}
+            />
+            <input type="submit" value="Search" style={{ color: "black" }} />
+          </form>
         </Box>
+        {/* <Box> */}
+        <Box className="auditoryTracksDiv">
+          {currentTracks.map((track) => (
+            <SingleTrack key={track.id} track={track} handlePlay={handlePlay} />
+          ))}
+        </Box>
+        {/* </Box> */}
         <Box className="pagination">
           <Paginate
             storeTracks={storeTracks}
